1d03edf7b7390566e08006cc07008eb0006a95c1
Egor Kazancev <3gork4z4ncev@gmail.com>
2021-11-02 17:56:35 +0500

    Chapter 9 (ViewModel and RecyclerView)

---
+++ app/src/main/java/com/egorkazantsev/criminalintent/CrimeListViewModel.kt
@@ -0,0 +1,17 @@
+package com.egorkazantsev.criminalintent
+
+import androidx.lifecycle.ViewModel
+
+class CrimeListViewModel : ViewModel() {
+
+    val crimes = mutableListOf<Crime>()
+
+    init {
+        for (i in 1..40) {
+            val crime = Crime()
+            crime.title = "Crime #$i"
+            crime.isSolved = i % 3 == 0
+            crimes += crime
+        }
+    }
+}
