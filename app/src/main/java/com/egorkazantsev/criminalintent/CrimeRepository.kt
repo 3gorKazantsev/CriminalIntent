5e9b96b7eedf436fe43f3e76378e680a6ad0d5f8
Egor Kazancev <3gork4z4ncev@gmail.com>
2021-11-05 20:25:55 +0500

    Chapter 12 (Database queries, fragment navigation)

--- app/src/main/java/com/egorkazantsev/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/egorkazantsev/criminalintent/CrimeRepository.kt
@@ -5,11 +5,13 @@ import androidx.room.Room
 import com.egorkazantsev.criminalintent.database.CrimeDatabase
 import java.lang.IllegalStateException
 import java.util.*
+import java.util.concurrent.Executors
 
 private const val DATABASE_NAME = "crime-database"
 
 class CrimeRepository private constructor(context: Context) {
 
+    // properties
     private val database: CrimeDatabase = Room.databaseBuilder(
         context.applicationContext,
         CrimeDatabase::class.java,
@@ -18,9 +20,24 @@ class CrimeRepository private constructor(context: Context) {
 
     private val crimeDao = database.crimeDao()
 
+    private val executor = Executors.newSingleThreadExecutor()
+
+    // functions
     fun getCrimes() = crimeDao.getCrimes()
 
     fun getCrime(id: UUID) = crimeDao.getCrime(id)
+
+    fun updateCrime(crime: Crime) {
+        executor.execute {
+            crimeDao.updateCrime(crime)
+        }
+    }
+
+    fun insertCrime(crime: Crime) {
+        executor.execute {
+            crimeDao.insertCrime(crime)
+        }
+    }
 
     companion object {
         private var INSTANCE: CrimeRepository? = null
